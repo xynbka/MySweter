2c974f8bbe5c2cbe43211efa1f84f1ce98f68e80
xynbka <74504359+xynbka@users.noreply.github.com>
2021-01-03 21:34:14 +0300

    Init commit

---
+++ src/main/java/com/example/sweater/Application.java
@@ -0,0 +1,13 @@
+package com.example.sweater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.BufferedReader;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
