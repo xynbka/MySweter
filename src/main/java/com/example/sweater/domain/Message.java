7a960c1bc36c714b09371c64c4fa392a9ec7e114
xynbka <74504359+xynbka@users.noreply.github.com>
2021-01-09 14:19:13 +0300

    Add Postgress, Filter

---
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -0,0 +1,48 @@
+package com.example.sweater.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+    private String tag;
+
+    public Message(String name, String tag) {
+        this.name = name;
+        this.tag = tag;
+    }
+
+    public Message() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+}
