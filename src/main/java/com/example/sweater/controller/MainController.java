48b2b34e79d3ccda77a326017c14e883807dea48
Roman <xynbka@gmail.com>
2021-01-12 21:28:30 +0300

    add Freemarker

--- src/main/java/com/example/sweater/controller/MainController.java
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -6,6 +6,7 @@ import com.example.sweater.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Map;
 
 @Controller
-public class GreetingsController {
+public class MainController {
     @Autowired
     private MessageRepo messageRepo;
 
@@ -23,9 +24,16 @@ public class GreetingsController {
     }
 
     @GetMapping("/main")
-    public String main(Map<String,Object> model){
+    public String main(@RequestParam(required = false) String filter, Model model){
         Iterable<Message> messages = messageRepo.findAll();
-        model.put("messages", messages);
+
+        if (filter != null && !filter.isEmpty())
+            messages = messageRepo.findByTag(filter);
+        else
+            messages = messageRepo.findAll();
+
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
         return "main";
     }
 
@@ -40,14 +48,6 @@ public class GreetingsController {
 
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
-        return "main";
-    }
-
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter, Map<String,Object> model) {
-        if (filter != null && filter != "") model.put("messages", messageRepo.findByTag(filter));
-        else model.put("messages", messageRepo.findAll());
-
         return "main";
     }
 
